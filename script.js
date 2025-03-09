a0e1201a0f755abf25e3c1259dd81bbc0feb274d
sahilstha123 <sthasahil401@gmail.com>
2025-03-09 18:49:04 +0545

    upload display clickevent

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const allbtnelms = document.querySelectorAll(".btn");
 let btnToDisplay = "";
+
+const displayElm = document.querySelector(".display");
+
+const buttonAction = () => {
+  const value = btn.innerText;
+  btnToDisplay += value;
+  display(btnToDisplay);
+};
+// attach click event to all buttons
 allbtnelms.forEach((btn) => {
   // console.log(btn)
   btn.addEventListener("click", () => {
-    const value = btn.innerText;
-    btnToDisplay += value;
-    console.log(btnToDisplay);
+    // console.log(btnToDisplay);
     // console.log(value);
   });
 });
+
+// update clicked button value to display area
+const display = (str) => {
+  displayElm.innerText = str;
+};
